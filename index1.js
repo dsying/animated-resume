14e38b05806e04af49453e77d4a8bcdb96eaef1b
dingsheng <317010370@qq.com>
2018-10-26 17:31:01 +0800

    会动的简历第而版 promise解决回调地狱 暂时有问题 待解决

--- index1.js
+++ index1.js
@@ -46,16 +46,39 @@ let result = `
   `
   let result2 = `
   #paper{
+    font-size:18px;
     flex-grow: 1;
     flex-basis: 50%;
     padding: 8px;
-    background:red;
+    margin: 0;
+    border: 5px solid black;
   }  
+  `
+
+  let md = `
+  # 自我介绍
+
+  我叫丁胜
+  1992年 1月 15日 出生
+  毕业于重庆西南大学
+  自学前端半年
+  希望应聘贵公司前端开发岗位
+
+  # 技能
+
+  熟悉JavaScript CSS 
+
+  # 项目介绍
+  
+  1 网易云音乐
+  2 简书网站模仿
   `
 
   writeCode('',result,()=>{
     createPaper(()=>{
-      writeCode(result,result2)
+      writeCode(result,result2,()=>{
+        writeMarkdown(md)
+      })
     })
   })
 
@@ -77,9 +100,21 @@ function writeCode(prefix,result,callback){
 
 
 function createPaper(callback) {
-  let paper = document.createElement('div')
+  let paper = document.createElement('pre')
   paper.id = 'paper'
-  paper.innerText = '第二部分准备完毕'
   document.body.appendChild(paper)
   callback()
+}
+
+function writeMarkdown(md){
+  let n = 0
+  let id = setInterval(() => {
+    n += 1
+    let markdown = md.substring(0, n)
+    paper.innerHTML = markdown
+    document.querySelector('#paper').scrollTop = 10000
+    if (n >= md.length) {
+      window.clearInterval(id)
+    }
+  }, 10)
 }
